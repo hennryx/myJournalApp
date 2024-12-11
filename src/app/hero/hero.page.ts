dfcc3805680f498bcf1a6aecd147ffba6ce0cb3b
Hennryx Samson <hennryx101@gmail.com>
2024-12-11 23:17:10 +0800

    update index

--- src/app/hero/hero.page.ts
+++ src/app/hero/hero.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { IonContent } from '@ionic/angular/standalone';
 
@@ -7,17 +8,28 @@ import { IonContent } from '@ionic/angular/standalone';
   templateUrl: 'hero.page.html',
   styleUrls: ['hero.page.scss'],
   standalone: true,
-  imports: [IonContent, RouterModule],
+  imports: [IonContent, RouterModule, CommonModule],
 })
-export class HeroPage {
+export class HeroPage implements OnInit {
+  username: string | null = null;
   constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.username = localStorage.getItem('userName');
+  }
 
   handleNavigate() {
+    this.username = localStorage.getItem('userName') || "";
     const nameInput = document.getElementById('name') as HTMLInputElement;
-    const name = nameInput.value.trim();
-
-    if (name) {
-      localStorage.setItem('userName', name);
+    
+    console.log("ey");
+    if(this.username) {
+      this.router.navigate(['tabs/home']);
+      return
+    }
+    
+    if (nameInput) {
+      localStorage.setItem('userName', nameInput.value);
+      this.username = nameInput.value;
       this.router.navigate(['tabs/home']);
     } else {
       alert('Please enter your name before proceeding.');
